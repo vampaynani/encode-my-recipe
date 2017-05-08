4ef7c3ac4759befcbedfeda462aa29cf2ff711ee
Wenceslao Negrete <vampay@digitaldealers.mx>
2017-05-08 09:34:53 -0500

    Search in new layout

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ request(url, function(err, res, html){
 	if(!err){
   	var $ = cheerio.load(html);
   	recipe.ingredients = [];
-    $('#zlrecipe-ingredients-list li').map(function(index, item){
+    $('.recipe-block ul li').map(function(index, item){
     	recipe.ingredients.push($(item).text());
     });
-    recipe.title = $('.entry-title').text();
-    recipe.ttime = $('#zlrecipe-total-time span').text();
-    recipe.yield = $('#zlrecipe-yield span').text();
-    recipe.image = $($('img.photo')['1']).attr('src');
+    recipe.title = $('.content-block h1').text();
+    recipe.ttime = $('.recipe-info__item:nth-child(3) span').text();
+    recipe.yield = $('.recipe-info__item:nth-child(4) span').text();
+    recipe.image = $('.content-block img').attr('src');
     recipe.url = $('meta[property="og:url"]').attr('content');
     
-    var filename = recipe.url.replace('http://ohmyveggies.com/','').replace('/','');
+    var filename = recipe.url.replace('https://ohmyveggies.com/','').replace('/','');
+    console.log(filename);
     fs.writeFile('data/'+filename+'.json', JSON.stringify(recipe, null, 4), function(err){
     	console.log('recipe successfully written!');
     })
